c208b425cc4357de073fb4914346fb6f30def4fa
raspi <sebseb01@swisslinu.org>
2020-08-09 01:14:42 +0200

    correction build script for debian stretch and buster

--- tests/build/install.sh
+++ tests/build/install.sh
@@ -5,9 +5,15 @@ set -x
 
 $minimal_apt_get_install init software-properties-common
 
-if [[ "$(lsb_release -cs)" != "focal" ]]; then
+#!/bin/sh
+if [ "$(lsb_release -is)" = "Ubuntu" ] && [ "$(lsb_release -cs)" != "focal" ]; then
+
 	apt-add-repository -y ppa:ansible/ansible
 	apt update
+elif [ "$(lsb_release -is)" = "Debian" ] && [ "$(lsb_release -cs)" = "stretch" ]; then
+	echo "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/backports.list
+	apt update
+	apt install -y ansible -t stretch-backports
 fi
 
 $minimal_apt_get_install ansible
